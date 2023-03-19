3c050b19a9e8c299ba11e09b9c955c69aa79093b
Lin Chen <136516935@qq.com>
2023-03-19 20:58:11 +0800

    服务端数据获取

--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,19 @@
   <h2>Index Page</h2>
   <!-- 嵌套组件名称 -->
   <FooBarButton></FooBarButton>
+  <!-- 数据获取 -->
+  <div v-for="todo in todos" :key="todo.id">
+    <input type="checkbox" v-model="todo.completed">
+    <strong>{{ todo.title }}</strong>
+  </div>
 </template>
+<script setup>
+  // useAsyncData 方式
+  // const { data: todos } = await useAsyncData(
+  //   'todos', () => $fetch('/api/todo')
+  // )
+
+  // useFetch 方式
+  const { data: todos } = await useFetch('/api/todo')
+</script>
+
---
+++ server/api/todo.ts
@@ -0,0 +1,9 @@
+const todos = [
+  { id: 1, title: "next3", completed: false },
+  { id: 2, title: "vue3", completed: true },
+];
+
+console.log(todos)
+export default () => {
+  return todos;
+};
